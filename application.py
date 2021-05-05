fefa021fa1700f64799336fcf57c02e5b2512681
gina-cyber <77301888+gina-cyber@users.noreply.github.com>
2021-05-05 13:31:01 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,85 @@
+Teams = [
 
+'Bandits’
+
+'Warriors’
+
+'Panthers’
+
+},
+
+{
+
+
+'Players’ = [{
+            'name’ = ‘James Arnold’
+            ‘guardian’ = ‘Liam Arnold and Matt Arnold’
+            'experience’= ‘Yes’
+            ‘height’= ‘40 inches’
+},
+
+{
+
+
+'name’ = ‘Clifford Smith’
+            ‘guardian’ = ‘Kyle Smith and Leo Smith’
+            'experience’= ‘Yes’
+            ‘height’= ‘42 inches’
+
+},
+
+{
+
+
+'name’ = ‘Liam Tremmonds’
+            ‘guardian’ = ‘Judah Tremmonds and Alfred Tremmonds’
+            'experience’= ‘No’
+            ‘height’= ‘41 inches’
+  
+  
+},
+
+{
+
+
+'name’ = ‘Ernest Harvey’
+            ‘guardian’ = ‘Jacob Harvey and Leon Harvey’
+            'experience’= ‘No’
+            ‘height’= ‘45 inches’
+
+},
+
+{
+
+
+'name’ = ‘Jerome Steel’
+            ‘guardian’ = ‘Lucas Steele and Derin Steele’
+            'experience’= ‘No’
+            ‘height’= ‘44 inches’
+
+},
+
+{
+  
+  'name’ = ‘Jerome Steel’
+            ‘guardian’ = ‘Lucas Steele and Derin Steele’
+            'experience’= ‘No’
+            ‘height’= ‘44 inches’
+  
+  },
+
+{
+  'name’ = ‘Tim Wilson’
+            ‘guardian’ = ‘Alfred Wilson and Jerry Wilson’
+            'experience’= ‘Yes’
+            ‘height’= ‘44 inches’
+  
+  
+},
+
+{
+  
+  'name’ = ‘Noah Clemmens’
+            ‘guardian’ = ‘Jacob Clemmens and Terry Clemmens’
+            'experience’= ‘No’
+            ‘height’= ‘44 inches’
