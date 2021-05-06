b3fa00b5ec70ded85f9a3238abce7d512162225e
gina-cyber <77301888+gina-cyber@users.noreply.github.com>
2021-05-06 10:28:00 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -10,29 +10,29 @@ Teams = [
 
 
 
-Players = [{'name' : 'James Arnold',
+Players = {'name' : 'James Arnold',
 'guardian': 'Jason Arnold',
              'height' : '40 inches',
-             'experience' : 'Yes'}]
+             'experience' : 'Yes'}
 },
 
 {
 
 
-'name’ = [{‘Clifford Smith’,
+'name’ = {‘Clifford Smith’,
 ‘guardian’: ‘Kyle Smith and Leo Smith’,
             'experience’= ‘Yes’
-            ‘height’= ‘42 inches’}]
+            ‘height’= ‘42 inches’}
 
 },
 
 {
 
 
-'name’ = [{‘Liam Tremmonds’
+'name’ = {‘Liam Tremmonds’
 ‘guardian’: ‘Judah Tremmonds and Alfred Tremmonds’
             'experience’= ‘No’
-            ‘height’= ‘41 inches’}]
+            ‘height’= ‘41 inches’}
   
   
 },
@@ -40,45 +40,45 @@ Players = [{'name' : 'James Arnold',
 {
 
 
-'name’ = [{‘Ernest Harvey’
+'name’ = {‘Ernest Harvey’
 ‘guardian’: ‘Jacob Harvey and Leon Harvey’
             'experience’= ‘No’
-            ‘height’= ‘45 inches’}]
+            ‘height’= ‘45 inches’}
 
 },
 
 {
 
 
-'name’ = [{‘Jerome Steel’
+'name’ = {‘Jerome Steel’
 ‘guardian': ‘Lucas Steele and Derin Steele’
             'experience’= ‘No’
-            ‘height’= ‘44 inches’}]
+            ‘height’= ‘44 inches’}
 
 },
 
 {
   
-  'name’ = [{‘Jerome Steel’
+  'name’ = {‘Jerome Steel’
   ‘guardian’: ‘Lucas Steele and Derin Steele’
             'experience’= ‘No’
-            ‘height’= ‘44 inches’}]
+            ‘height’= ‘44 inches’}
   
   },
 
 {
-  'name’ = [{‘Tim Wilson’
+  'name’ = {‘Tim Wilson’
   ‘guardian’: ‘Alfred Wilson and Jerry Wilson’
             'experience’= ‘Yes’
-            ‘height’= ‘44 inches’}]
+            ‘height’= ‘44 inches’}
   
   
 },
 
 {
   
-  'name’ = [{‘Noah Clemmens’
+  'name’ = {‘Noah Clemmens’
   ‘guardian’: ‘Jacob Clemmens and Terry Clemmens’
             'experience’= ‘No’
-            ‘height’= ‘44 inches’}]
+            ‘height’= ‘44 inches’}
 
