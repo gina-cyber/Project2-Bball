92d633c48e1725ad45143043c40459319470fee2
gina-cyber <77301888+gina-cyber@users.noreply.github.com>
2021-05-11 14:20:11 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,86 +1,114 @@
-Teams = [
+TEAMS = [
+    'Panthers',
+    'Bandits',
+    'Warriors',
+]
 
-'Bandits’
-
-'Warriors’
-
-'Panthers’
-
-],
-
-
-
-Players = [{'name' : 'James Arnold',
-'guardian': 'Jason Arnold',
-             'height' : '40 inches',
-           #make an Integer
-             'experience' : 'Yes'
-          #make a boolean=false}
-},
-
-{
-
-
-'name’ = {‘Clifford Smith’,
-‘guardian’: ‘Kyle Smith and Leo Smith’,
-            'experience’= ‘Yes’
-            ‘height’= ‘42 inches’}
-
-},
-
-{
-
-
-'name’ = {‘Liam Tremmonds’
-‘guardian’: ‘Judah Tremmonds and Alfred Tremmonds’
-            'experience’= ‘No’
-            ‘height’= ‘41 inches’}
-  
-  
-},
-
-{
-
-
-'name’ = {‘Ernest Harvey’
-‘guardian’: ‘Jacob Harvey and Leon Harvey’
-            'experience’= ‘No’
-            ‘height’= ‘45 inches’}
-
-},
-
-{
-
-
-'name’ = {‘Jerome Steel’
-‘guardian': ‘Lucas Steele and Derin Steele’
-            'experience’= ‘No’
-            ‘height’= ‘44 inches’}
-
-},
-
-{
-  
-  'name’ = {‘Jerome Steel’
-  ‘guardian’: ‘Lucas Steele and Derin Steele’
-            'experience’= ‘No’
-            ‘height’= ‘44 inches’}
-  
-  },
-
-{
-  'name’ = {‘Tim Wilson’
-  ‘guardian’: ‘Alfred Wilson and Jerry Wilson’
-            'experience’= ‘Yes’
-            ‘height’= ‘44 inches’}
-  
-  
-},
-
-{
-  
-  'name’ = {‘Noah Clemmens’
-  ‘guardian’: ‘Jacob Clemmens and Terry Clemmens’
-            'experience’= ‘No’
-            ‘height’= ‘44 inches’}]
-
+PLAYERS = [{
+        'name': 'Karl Saygan',
+        'guardians': 'Heather Bledsoe',
+        'experience': 'YES',
+        'height': '42 inches'
+    },
+    {
+        'name': 'Matt Gill',
+        'guardians': 'Charles Gill and Sylvia Gill',
+        'experience': 'NO',
+        'height': '40 inches'
+    },
+    {   'name': 'Sammy Adams',
+        'guardians': 'Jeff Adams and Gary Adams',
+        'experience': 'NO',
+        'height': '45 inches'
+    },
+    {
+        'name': 'Chloe Alaska',
+        'guardians': 'David Alaska and Jamie Alaska',
+        'experience': 'NO',
+        'height': '47 inches'
+    },
+    {
+        'name': 'Bill Bon',
+        'guardians': 'Sara Bon and Jenny Bon',
+        'experience': 'YES',
+        'height': '43 inches'
+    },
+    {
+        'name': 'Joe Kavalier',
+        'guardians': 'Sam Kavalier and Elaine Kavalier',
+        'experience': 'NO',
+        'height': '39 inches'
+    },
+    {
+        'name': 'Phillip Helm',
+        'guardians': 'Thomas Helm and Eva Jones',
+        'experience': 'YES',
+        'height': '44 inches'
+    },
+    {
+        'name': 'Les Clay',
+        'guardians': 'Wynonna Brown',
+        'experience': 'YES',
+        'height': '42 inches'
+    },
+    {
+        'name': 'Sal Dali',
+        'guardians': 'Gala Dali',
+        'experience': 'NO',
+        'height': '41 inches'
+    },
+    {
+        'name': 'Suzane Greenberg',
+        'guardians': 'Henrietta Dumas',
+        'experience': 'YES',
+        'height': '44 inches'
+    },
+    {
+        'name': 'Jill Tanner',
+        'guardians': 'Mark Tanner',
+        'experience': 'YES',
+        'height': '36 inches'
+    },
+    {
+        'name': 'Arnold Willis',
+        'guardians': 'Claire Willis',
+        'experience': 'NO',
+        'height': '43 inches'
+    },
+    {
+        'name': 'Herschel Krustofski',
+        'guardians': 'Hyman Krustofski and Rachel Krustofski',
+        'experience': 'YES',
+        'height': '45 inches'
+    },
+    {
+        'name': 'Eva Gordon',
+        'guardians': 'Wendy Martin and Mike Gordon',
+        'experience': 'NO',
+        'height': '45 inches'
+    },
+    {
+        'name': 'Ben Finkelstein',
+        'guardians': 'Aaron Lanning and Jill Finkelstein',
+        'experience': 'NO',
+        'height': '44 inches'
+    },
+    {
+        'name': 'Joe Smith',
+        'guardians': 'Jim Smith and Jan Smith',
+        'experience': 'YES',
+        'height': '42 inches'
+    },
+    {
+        'name': 'Diego Soto',
+        'guardians': 'Robin Soto and Sarika Soto',
+        'experience': 'YES',
+        'height': '41 inches'
+    },
+    {
+        'name': 'Kimmy Stein',
+        'guardians': 'Bill Stein and Hillary Stein',
+        'experience': 'NO',
+        'height': '41 inches'
+    }
+]
